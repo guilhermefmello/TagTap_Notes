0edbca99f7b31d0a1c76ea456581f170e67dcbf6
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2017-06-03 14:57:34 +0100

    Creating method to create NDEF message

--- app/src/main/java/guilherme/tagtapnotes/MainActivity.java
+++ app/src/main/java/guilherme/tagtapnotes/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -67,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    // Writing the message into the NFC TAG
     private void write(String text, Tag tag) throws IOException, FormatException {
 
         NdefRecord[] records = { createRecord(text) };
@@ -82,8 +83,26 @@ public class MainActivity extends AppCompatActivity {
         ndef.close();
     }
 
+    // Creating NDEF Message to write into the NFC TAG
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+        String lang       = "en";
+        byte[] textBytes  = text.getBytes();
+        byte[] langBytes  = lang.getBytes("US-ASCII");
+        int    langLength = langBytes.length;
+        int    textLength = textBytes.length;
+        byte[] payload    = new byte[1 + langLength + textLength];
 
+        // set status byte (see NDEF spec for actual bits)
+        payload[0] = (byte) langLength;
 
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1,              langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
+
+        return recordNFC;
+    }
 
 
 
